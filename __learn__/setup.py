28fdad0b007defbae8b82d6414551b84944a6801
xodiumluma <28709874+xodiumluma@users.noreply.github.com>
2024-04-12 12:17:28 +1000

    [learn] default versions

--- __learn__/setup.py
+++ __learn__/setup.py
@@ -21,4 +21,12 @@ from setuptools import setup
 from setuptools.command.test import test as TestCommandBase
 from setuptools.dist import Distribution
 
+# add version.py to sys.path to make it possible to import it directly
+version_path = os.path.join(os.path.dirname(__file__), 'tf_agents')
+sys.path.append(version_path)
+import version as tf_agents_version # pylint: disable=g-import-not-at-top
 
+# what we set as default versions for packages
+# they can be overridden for testing/RCs (and they are often overridden - with flags)
+TFP_VERSION = 'tensorflow-probability'
+TFP_NIGHTLY = 'tfp-nightly'
