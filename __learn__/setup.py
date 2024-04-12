0dd0ebd757acc7e6ec51c4cd53d70d9c5e6efe93
xodiumluma <28709874+xodiumluma@users.noreply.github.com>
2024-04-12 21:03:49 +1000

    [learn] continue default package versions

--- __learn__/setup.py
+++ __learn__/setup.py
@@ -30,3 +30,11 @@ import version as tf_agents_version # pylint: disable=g-import-not-at-top
 # they can be overridden for testing/RCs (and they are often overridden - with flags)
 TFP_VERSION = 'tensorflow-probability'
 TFP_NIGHTLY = 'tfp-nightly'
+TENSORFLOW_VERSION = 'tensorflow'
+TENSORFLOW_NIGHTLY = 'tf-nightly'
+REVERB_VERSION = 'dm-reverb'
+REVERB_NIGHTLY = 'dm-reverb-nightly'
+RLDS_VERSION = 'rlds'
+TF_KERAS_VERSION = 'tf-keras'
+# @TODO(b/224850217): there aren't any rlds nightly builds
+RLDS_NIGHTLY = 'rlds'
